ec876f442ab0196db23c0aba32a69fde46ce9c5d
craigmwarner1 <craigmwarner1@gmail.com>
2016-05-24 13:52:29 -0400

    Changed filename

---
+++ workspace/test.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int main() {
+    
+    int lmnop[3];
+    
+    int* p;
+    
+    scanf("%i", &lmnop[0]);
+    scanf("%i", &lmnop[1]);
+    
+    //p = &lmnop[0];
+    printf("%i", &lmnop[0]);
+    
+}
